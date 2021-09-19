d3fdcb880501050ced883bb365e0cee0a7d76307
oabecerrago <55248062+oabecerrago@users.noreply.github.com>
2021-09-18 21:49:13 -0500

    Add files via upload

---
+++ TurnoRest.App/TurnoRest.App.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TurnoRest.App.Dominio;
+
+namespace TurnoRest.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Entity Framework!");
+        }
+    }
+}
---
+++ TurnoRest.App/TurnoRest.App.Dominio/Entidades/Persona.cs
@@ -0,0 +1,21 @@
+using System;
+namespace TurnoRest.App.Dominio
+{
+    public class Persona
+    {
+       
+       public int Id{get;set;}
+       
+       public string Nombre{get;set;}
+
+       public string Apellido{get;set}
+
+       public int Identificacion{get;set;}
+
+       public int edad{get;set;}
+
+       public string statusCOVID{get;set;}
+
+       } 
+    }
+}
